d47552515993207ded4c18b300fc14d0ada8d0cb
Heloísa Marcovecchio <hrozzette@gmail.comr>
2025-11-06 16:53:42 -0300

    corrigindo

--- src/routes.ts
+++ src/routes.ts
@@ -1,35 +1,16 @@
 import { Router } from "express"; 
 import type { Request, Response } from "express"; 
 import ClienteController from "./controller/ClientesController.js";
-import MedicamentoController from "./controller/MedicamentosController.js";
 
-
-
-const router = Router(); 
+const router = Router();
 
 router.get("/api", (req: Request, res: Response) => {
-    res.status(200).json({ mensagem: "Olá, seja bem-vindo!" });
+    res.status(200).json({ mensagem: "Olá, seja bem-vindo" });
 });
 
-/**
 
- */
 router.get("/api/clientes", ClienteController.todos);
+
 router.post("/api/clientes", ClienteController.novo);
-router.get("/api/clientes/:cpf", ClienteController.cliente);
-
-/**
-
- */
-router.get("/api/medicamentos", MedicamentoController.todos);
-router.get(
-    "/api/medicamentos/nome/:nome",
-    MedicamentoController.nomeMedicamento
-);
-router.get(
-    "/api/medicamentos/principio/:principioAtivo",
-    MedicamentoController.principioMedicamento
-);
-router.post("/api/medicamentos", MedicamentoController.novo);
 
 export { router }; 
